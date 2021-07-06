e0051a9f7409a7259184c17c9bc04102c2992b42
mpilopillz <mpilo.dlamini@yahoo.com>
2021-07-06 04:37:36 +0200

    learning debug and pause

--- cypressPlayground/cypress/integration/cheatSheet/cyCheatSheet.js
+++ cypressPlayground/cypress/integration/cheatSheet/cyCheatSheet.js
@@ -10,6 +10,12 @@ describe("List of Commands to test effectivly", () => {
     cy.get(".product:visible").should("have.length", 4); // incase there are some invisible elements
     cy.wait(2000);
     cy.get(".products").find(".fruit").should("have.lenth", 10); // inside products, find 'fruit' should have a list of 4 fruits
-    cy.get(".products").find(".fruit").eq(2).contains("ADD TO CART").click(); // checks for the second (might click the 3rd) item on the search and gets  it add to card button
+    cy.pause();
+    cy.get(".products")
+      .find(".fruit")
+      .eq(2)
+      .contains("ADD TO CART")
+      .click()
+      .debug(); // checks for the second (might click the 3rd) item on the search and gets  it add to card button
   });
 });
